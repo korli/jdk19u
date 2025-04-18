811c53beec7750122de57ba6755a0f6377ec04f9
Powerm1nt <leonard.chabot@nuka.works>
2025-04-18 03:06:06 +0200

    Trying to fix specific os::print_tos_pc(outputStream*, void*)

--- src/hotspot/os_cpu/haiku_x86/os_haiku_x86.cpp
+++ src/hotspot/os_cpu/haiku_x86/os_haiku_x86.cpp
@@ -506,22 +506,22 @@ void os::print_context(outputStream *st, const void *context) {
   st->cr();
 }
 
-//void os::print_tos_pc(outputStream *st, const void *context) {
-//  if (context == NULL) return;
-//
-//  const ucontext_t* uc = (const ucontext_t*)context;
-//
-//  address sp = (address)os::Haiku::ucontext_get_sp(uc);
-//  print_tos(st, sp);
-//  st->cr();
-//
-//  // Note: it may be unsafe to inspect memory near pc. For example, pc may
-//  // point to garbage if entry point in an nmethod is corrupted. Leave
-//  // this at the end, and hope for the best.
-//  address pc = os::Posix::ucontext_get_pc(uc);
-//  print_instructions(st, pc);
-//  st->cr();
-//}
+void os::print_tos_pc(outputStream *st, const void *context) {
+  if (context == NULL) return;
+
+  const ucontext_t* uc = (const ucontext_t*)context;
+
+  address sp = (address)os::Haiku::ucontext_get_sp(uc);
+  print_tos_pc(st, sp);
+  st->cr();
+
+  // Note: it may be unsafe to inspect memory near pc. For example, pc may
+  // point to garbage if entry point in an nmethod is corrupted. Leave
+  // this at the end, and hope for the best.
+  address pc = os::Posix::ucontext_get_pc(uc);
+  os::print_instructions(st, pc, sizeof(char));
+  st->cr();
+}
 
 void os::print_register_info(outputStream *st, const void *context) {
   if (context == NULL) return;
